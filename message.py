a3015caf1e823102096e240936d677af5ffaa077
Паша Яромский <pashayaromsky@yandex.by>
2022-09-17 20:09:50 +0300

    100

--- message.py
+++ message.py
@@ -5,8 +5,8 @@ msg = f.read()
 f.close
 
 root = tk.Tk()
-root.geometry('600x400')
+# root.geometry('600x400')
 root['bg'] = 'white'
-label = tk.Label(fg = "DeepSkyBlue2", bg = "white", width = 50, font = "Calibri 30", text=msg)
+label = tk.Label(fg = "DeepSkyBlue2", bg = "white", width = 100, font = "Calibri 30", text=msg)
 label.pack()
 root.mainloop()
