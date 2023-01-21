0980c3df7542cb2d428958d2e76c74bfaba75f80
trbcon <pashayaromsky@yandex.by>
2023-01-21 16:34:05 +0300

    add cmd scripts

--- tcas.py
+++ tcas.py
@@ -1,19 +1,49 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic    
 import socket, os
 from tkinter.colorchooser import askcolor
 
-def get_var():
-    number = ui.compL.currentText()
-    command = ui.commL.currentText()
-    if command == "message":
-        command = "!" + ui.line.text()
-    elif command == "cmd" or command == "rpl" or command == "app_off" or command == "app_on":
-        command = command + " " + ui.line.text()
-    if number != "all":
-        number = int(number)
-    cipter(command, number)
+# ip.txt, cfs.txt, sdft.txt, scr.txt 
 
-conf = open("cfg.txt", "r")
+def get_var():
+	bat_scrpit = 0
+	number = ui.compL.currentText()
+	command = ui.commL.currentText()
+	if command == "message":
+		command = "!" + ui.line.text()
+	elif command == "cmd" or command == "rpl" or command == "app_off" or command == "app_on":
+		command = command + " " + ui.line.text()
+	elif command == "script":
+		bat_scrpit = 1
+		script_file = open("scr.txt", "w")
+		script_file.close()
+		os.system("scr.txt")
+		tr_sc = 0
+		while True:
+			if tr_sc == 0:
+				script_file = open("scr.txt", "r")
+				data_script_file = script_file.readline()
+				script_file.close()
+				if data_script_file != "":
+					tr_sc = 1
+					script_file = open("scr.txt", "r")
+			else:
+				if number != "all":
+					number = int(number)
+				try:
+					if tr_sc == 1:
+						cipter("scr/;/;0 start", number)
+						tr_sc += 1
+					cipter(("scr/;/;0 " + script_file.readline()), number)
+				except:
+					cipter("scr/;/;0 stop/end__2qpw", number)
+					break
+
+	if number != "all":
+		number = int(number)
+	if bat_scrpit == 0:
+		cipter(command, number)
+
+"""conf = open("cfg.txt", "r")
 fore_grount = conf.readline().replace("\n", "")
 back_ground = conf.readline().replace("\n", "")
 conf.close()
@@ -43,43 +73,43 @@ def bg_save():
 	conf.close()
 	conf = open("cfg.txt", "w")
 	conf.write(fg_color + bg_color + "\n")
-	conf.close()
+	conf.close()"""
 
 def cipter(text, number):
 	if "!" in text:                      #шифрование текста для корректного вывода кирилицы
-		text = text.replace("а", "1 ")
-		text = text.replace("б", "2 ")
-		text = text.replace("в", "3 ")
-		text = text.replace("г", "4 ")
-		text = text.replace("д", "5 ")
-		text = text.replace("е", "6 ")
-		text = text.replace("ё", "7 ")
-		text = text.replace("ж", "8 ")
-		text = text.replace("з", "9 ")
-		text = text.replace("и", "10 ")
-		text = text.replace("й", "11 ")
-		text = text.replace("к", "12 ")
-		text = text.replace("л", "13 ")
-		text = text.replace("м", "14 ")
-		text = text.replace("н", "15 ")
-		text = text.replace("о", "16 ")
-		text = text.replace("п", "17 ")
-		text = text.replace("р", "18 ")
-		text = text.replace("с", "19 ")
-		text = text.replace("т", "20 ")
-		text = text.replace("у", "21 ")
-		text = text.replace("ф", "22 ")
-		text = text.replace("х", "23 ")
-		text = text.replace("ц", "24 ")
-		text = text.replace("ч", "25 ")
-		text = text.replace("ш", "26 ")
-		text = text.replace("щ", "27 ")
-		text = text.replace("ъ", "28 ")
-		text = text.replace("ы", "29 ")
-		text = text.replace("ь", "30 ")
-		text = text.replace("э", "31 ")
-		text = text.replace("ю", "32 ")
-		text = text.replace("я", "33 ")
+		text = text.replace("а", "/1 ")
+		text = text.replace("б", "/2 ")
+		text = text.replace("в", "/3 ")
+		text = text.replace("г", "/4 ")
+		text = text.replace("д", "/5 ")
+		text = text.replace("е", "/6 ")
+		text = text.replace("ё", "/7 ")
+		text = text.replace("ж", "/8 ")
+		text = text.replace("з", "/9 ")
+		text = text.replace("и", "/10 ")
+		text = text.replace("й", "/11 ")
+		text = text.replace("к", "/12 ")
+		text = text.replace("л", "/13 ")
+		text = text.replace("м", "/14 ")
+		text = text.replace("н", "/15 ")
+		text = text.replace("о", "/16 ")
+		text = text.replace("п", "/17 ")
+		text = text.replace("р", "/18 ")
+		text = text.replace("с", "/19 ")
+		text = text.replace("т", "/20 ")
+		text = text.replace("у", "/21 ")
+		text = text.replace("ф", "/22 ")
+		text = text.replace("х", "/23 ")
+		text = text.replace("ц", "/24 ")
+		text = text.replace("ч", "/25 ")
+		text = text.replace("ш", "/26 ")
+		text = text.replace("щ", "/27 ")
+		text = text.replace("ъ", "/28 ")
+		text = text.replace("ы", "/29 ")
+		text = text.replace("ь", "/30 ")
+		text = text.replace("э", "/31 ")
+		text = text.replace("ю", "/32 ")
+		text = text.replace("я", "/33 ")
 
 	print(text)
 	
